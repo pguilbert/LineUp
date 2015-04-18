43006f698efb76c6bbe58521e38f55d976ada6f1
Paul GUILBERT <guilbert.paul@gmail.com>
2015-04-18 15:00:48 +0200

    Add some css

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 $('document').ready(function() {
-	var options = { videoId: 'cOfHQsVw2xA' };
-	$('#wrapper').tubular(options);
+    
+    var videos = [ 'UqLRqzTp6Rk',  'jvipPYFebWc', 'AOPMlIIg_38'];
+	var options = { videoId: videos[Math.floor(Math.random()*videos.length)] };
+	$('.youtube-video').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
 });
