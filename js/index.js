b513141dbbc79b73ca2ac3e607d3740c1b29dfdd
Paul GUILBERT <guilbert.paul@gmail.com>
2015-04-18 12:33:37 +0200

    First commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'cOfHQsVw2xA' };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
