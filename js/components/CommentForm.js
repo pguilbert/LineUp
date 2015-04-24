1aa563a96fcabed4eda9152a39289893c70b9047
Paul GUILBERT <guilbert.paul@gmail.com>
2015-04-24 17:32:43 +0200

    Make this work... move jquery autocomplete to react

--- js/components/CommentForm.js
+++ js/components/CommentForm.js
@@ -1,10 +1,13 @@
 var CommentForm = React.createClass({
     handleSubmit: function(e) {
         e.preventDefault();
-        var text = React.findDOMNode(this.refs.text).value.trim();
+        /* var text = React.findDOMNode(this.refs.text).value.trim();
         if (!text) {
             return;
         }
+        this.addItem(text); */
+    },
+    addItem: function(text) {
         this.props.onCommentSubmit( { text: text } );
         React.findDOMNode(this.refs.text).value = '';
     },
@@ -15,5 +18,34 @@ var CommentForm = React.createClass({
                     placeholder="Your favourite artist here..." ref="text" id={this.props.id}/>
             </form>
         );
+    },
+    componentDidMount: function() {
+        var that = this;
+        $(React.findDOMNode(this.refs.text)).autocomplete({
+            source : function(request, response){ 
+            $.ajax({
+                    url : 'proxy.php',
+                    crossDomain: true,
+                    dataType: 'json',
+                    data : {
+                        csurl: 'http://ws.audioscrobbler.com/2.0/',
+                        method : 'artist.search',
+                        limit : 20,
+                        format: 'json',
+                        artist: request.term
+                    },
+                    success : function(data){
+                        
+                        response($.map(data.results.artistmatches.artist, function(object){
+                            return object.name;
+                        }));
+                    }
+                });
+            },
+            select: function( event, ui ) {
+                event.preventDefault();
+                that.addItem(ui.item.value);
+            }
+        });
     }
 });
